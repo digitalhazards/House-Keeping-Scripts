41ae2fe7f676a9efcf018bdf2004f3fcf1205c80
TheFonix <actualfriction@gmail.com>
2017-06-09 13:45:23 +0100

    Fix Echo's

--- bigfilecleanup.sh
+++ bigfilecleanup.sh
@@ -10,35 +10,35 @@ EXT2="*.log"
 EXT3="*.zip"
 
 sleep 1
-echo""
-echo""
-echo""
-echo"--------------------------------------"
-echo"Digital Hazards House Keeping Scripts!"
-echo"--------------------------------------"
-echo""
-echo""
-echo""
+echo ""
+echo ""
+echo ""
+echo "--------------------------------------"
+echo "Digital Hazards House Keeping Scripts!"
+echo "--------------------------------------"
+echo ""
+echo ""
+echo ""
 sleep 2
 
-echo"Starting search for files bigger than $SIZE with the file extension $EXT1"
+echo "Starting search for files bigger than $SIZE with the file extension $EXT1"
 
 find $LOC -size +$SIZE -name "$EXT1" -exec rm -f {} \;
 
-echo"Starting search for files bigger than $SIZE with the file extension $EXT2"
+echo "Starting search for files bigger than $SIZE with the file extension $EXT2"
 
 find $LOC -size +$SIZE -name "$EXT2" -exec rm -f {} \;
 
-echo"Starting search for files bigger than $SIZE with the file extension $EXT3"
+echo "Starting search for files bigger than $SIZE with the file extension $EXT3"
 
 find $LOC -size +$SIZE -name "$EXT3" -exec rm -f {} \;
 
-echo""
-echo""
-echo""
-echo"--------------------------------------"
-echo" Script Complete! Thank you very much!"
-echo"--------------------------------------"
-echo""
-echo""
-echo""
+echo ""
+echo ""
+echo ""
+echo "--------------------------------------"
+echo " Script Complete! Thank you very much!"
+echo "--------------------------------------"
+echo ""
+echo ""
+echo ""
